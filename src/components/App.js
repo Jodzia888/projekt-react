482b73dcc66d66dcd485d15f328bf4a6c5d8b910
Szymujooo <sz.p505@wp.pl>
2024-11-19 11:18:35 +0100

    ostatnia lekcja

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Form from './Form';
+import Result from './Result';
+
+  function App() {
+    const [city, setCity] = useState("")
+    const [wetherData, setWetherData] = useState(null)
+
+
+    useEffect(()=>{
+      getWetherData()
+    },[city])
+
+    function getWetherData(){
+
+    }
+
+    return (
+      <div className="App">
+        <Form value={city} setValue={setCity} />
+        {city}
+        <Result wetherData={wetherData}/>
+    </div>
+    );
+  }
+
+
+export default App;
---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Result = ({wetherData}) => {
+    return(
+        <div>Miasto, pogoda</div>
+    )
+}
+
+export default Result;
